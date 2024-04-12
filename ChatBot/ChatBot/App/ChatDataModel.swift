2c20f61676c14ba0283100c0a7203e356aad5e80
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2024-04-12 13:55:00 +0900

    feat: ChatDataModel 추가

--- ChatBot/ChatBot/App/ChatDataModel.swift
+++ ChatBot/ChatBot/App/ChatDataModel.swift
@@ -1,8 +1,18 @@
-//
-//  ChatDataModel.swift
-//  ChatBot
-//
-//  Created by 윤진영 on 4/12/24.
-//
-
 import Foundation
+
+enum MessageType: String {
+    case question
+    case answer
+}
+
+struct ChatDataModel: Identifiable {
+    let id: UUID
+    let content: String
+    let messageType: MessageType
+    
+    init(id: UUID, content: String, messageType: MessageType) {
+        self.id = id
+        self.content = content
+        self.messageType = messageType
+    }
+}
